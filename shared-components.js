9a5cdf30f8f45bb5f8d3184ba9f219fabab396f8
Klaudia Rudna <klaudia@mac.home>
2026-05-11 13:37:46 +0100

    home and contact for perm role

--- shared-components.js
+++ shared-components.js
@@ -26,11 +26,29 @@ function getNavigationHTML(basePath) {
           <div></div>
         </div>
         <ul>
-          <li><a href="${basePath}services.html" data-page="services">Services</a></li>
-          <li><a href="${basePath}articles.html" data-page="articles">Articles</a></li>
           <li><a href="${basePath}case-studies.html" data-page="case-studies">Case Studies</a></li>
+          <li><a href="${basePath}articles.html" data-page="articles">Articles</a></li>
           <li><a href="${basePath}about.html" data-page="about">About</a></li>
           <li><a href="${basePath}contact.html" data-page="contact">Contact</a></li>
+          <li class="nav-linkedin">
+    <a 
+      href="https://www.linkedin.com/in/klaudia-rudna-6465a1a2"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="LinkedIn"
+      class="nav-linkedin-link"
+    >
+      <svg 
+        width="18" 
+        height="18" 
+        viewBox="0 0 24 24" 
+        fill="currentColor" 
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+      </svg>
+    </a>
+  </li>
         </ul>
       </nav>
     </header>
